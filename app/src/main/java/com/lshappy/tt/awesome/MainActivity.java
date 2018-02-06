b254b39d186bbc0a89541d4fbfb1c294b7bb1153
yi.liu <liuyi.lj@foxmail.com>
2018-02-06 23:00:44 +0800

    Initial commit

---
+++ app/src/main/java/com/lshappy/tt/awesome/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lshappy.tt.awesome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
